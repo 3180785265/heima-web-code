70b8b509365e1c3075d9bf03894b2bfe8a4edbb4
liujj <3180785265@qq.com>
2022-08-09 17:48:35 +0530

    es6 模块化，默认导入，默认导出 按需导入，按需导出 立即执行模块化代码

---
+++ 04-webpack-vue/01-es6的导入导出模块化/05-默认导入.js
@@ -0,0 +1,26 @@
+
+/* 
+   1.  从'./04-默认导出'这个js模块中 导入立马export default
+       向往共享的变量并使用 m1 成员接收里面 export default 向外共享的成员 */
+import m1 from './04-默认导出.js'
+
+
+
+
+console.log(m1)
+
+
+
+// 2. 在控制台 ， 使用 node.js  执行 这个模块化的js查看执行结果, 命令:  node ./05-默认导入.js
+
+
+
+
+
+/* 
+    默认导入的语法： 
+     import 接收名称 form '模块路径.js'
+
+
+    
+*/
---
+++ 04-webpack-vue/01-es6的导入导出模块化/06-按需导出.js
@@ -0,0 +1,30 @@
+
+// 
+
+
+//1. 向外按需导出变量
+export let s1 = 'abc';
+export let s2 = 'efg';
+
+
+// 2.向外 按需 导出方法 
+export function say() {
+
+}
+// 3.用函数表达式 导出 
+export let fn = () => {
+
+}
+
+
+export default {
+
+}
+
+
+
+/* 
+
+    按需导出的语法: export 按需导出的成员  
+
+*/

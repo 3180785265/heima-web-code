52143ea2e2e294db9b5eba59edd37e151c11afb3
liujj <3180785265@qq.com>
2022-08-10 04:26:37 +0530

    webpack 的 入口和出口

--- 04-webpack-vue/03-webpack/webpack-01/src/index.js
+++ 04-webpack-vue/03-webpack/webpack-01/src/index.js
@@ -2,7 +2,7 @@ import $ from "jquery";
 
 
 $(function () {
-    /* 选中奇数 */
+    // 选中奇数 
     $('li:odd').css('backgroundColor', 'pink')
     $('li:even').css('backgroundColor', 'red')
 
--- 04-webpack-vue/03-webpack/webpack-01/webpack.config.js
+++ 04-webpack-vue/03-webpack/webpack-01/webpack.config.js
@@ -1,20 +1,19 @@
 const path = require('path');
-model.exports = {
+
+module.exports = {
     //1.用来指定构建模式,
     // development: 开发模式进行构建：转化的代码不会进行压缩与混淆，但会解决比如js的兼容性问题(时间短)
     //production：发布上线模式：进行构建会对转换的代码压缩与混淆(时间长)
 
-    model: "development",
+    mode: "development",
 
     // 2.设置打包的入口与出口
-    entry: path.join(__dirname, './src/index.js') //path.join(__dirname, './src/index.js')表示绝对路径
 
-    ,
-    //出口
-    oninput: {
-        path: path.join(__dirname, './src/index.js'),//输出文件的存放路径
-        filename: 'bundle.js' // 输出文件的名称
-    }
+    entry: path.join(__dirname, './src/index.js'), // 打包入口文件的路径
+    output: { // 出口
+      path: path.join(__dirname, './dist'), //输出文件的存放路径
+      filename: 'bundle.js',
+    },
 
 }
 
@@ -28,5 +27,12 @@ model.exports = {
    
    2.     //定义入口和出口
    "entry": ""
+
+   3.设置打包的入口和出口
+    entry: path.join(__dirname, './src/index.js'), // 打包入口文件的路径
+    output: { // 出口
+      path: path.join(__dirname, './dist'), //输出文件的存放路径
+      filename: 'bundle.js',
+    },
    
 */

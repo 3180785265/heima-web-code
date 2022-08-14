36bf032ffe6fb96bc8bcf0b9108eca34218dafa7
liujj <3180785265@qq.com>
2022-08-14 19:26:43 +0800

    1

---
+++ 04-webpack-vue/04-vue/vue-02/src/components/List.vue
@@ -0,0 +1,33 @@
+<template>
+    <ul class="my-product">
+        <li v-for="(item, index) in arr" :key="index">
+            <span>{{ item.proname }}</span>
+            <span>{{ item.proprice }}</span>
+        </li>
+    </ul>
+</template>
+
+<script>
+// 1.引入空白Vue对象(EventBus)
+// 2.接收方- $on监听事件
+import eventBus from '@/EventBus'
+export default {
+    props: ['arr'],
+    //   3.组件创建完毕，监听send事件
+    created() {
+        eventBus.$on('send', (index, price) => {
+            
+        })
+    },
+}
+</script>
+
+<style>
+.my-product {
+    width: 400px;
+    padding: 20px;
+    border: 2px solid #000;
+    border-radius: 5px;
+    margin: 10px;
+}
+</style>

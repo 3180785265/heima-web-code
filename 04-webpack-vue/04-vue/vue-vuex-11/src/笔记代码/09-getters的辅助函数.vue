5af6de2dbf1f72e2853bbaa302644efb265b95b7
liujj <3180785265@qq.com>
2022-12-27 13:39:20 +0700

    666

--- 04-webpack-vue/04-vue/vue-vuex-11/src/笔记代码/09-getters的辅助函数.vue
+++ 04-webpack-vue/04-vue/vue-vuex-11/src/笔记代码/09-getters的辅助函数.vue
@@ -26,5 +26,19 @@ export default {
 }
 </script>
   <!-- 
-   
+        计算属性使用辅助函数的优点：
+
+          //1. 在导入下面这段代码的时候
+           computed: {
+            ...mapGetters(['fileList'])
+          },
+
+          //2. 辅助函数就会帮助我们自己写出这段代码, 简化了代码，起到了解耦
+            computed: {
+              
+              fileList(){
+                return   $store.getters.fileList
+              }
+
+            }
      -->

9ad7fa5ef775f62a4fd8fcfdf3ae0ddf6e5fb0f8
liujj <3180785265@qq.com>
2022-12-27 19:49:08 +0700

    侦听器笔记优化

--- 04-webpack-vue/04-vue/vue-01/src/学习笔记/29-侦听器-深度侦听-引用类型.vue
+++ 04-webpack-vue/04-vue/vue-01/src/学习笔记/29-侦听器-深度侦听-引用类型.vue
@@ -20,9 +20,11 @@ export default {
     watch: {
         // 侦听器，深度监听引用数据类型的属性
          user:{
-            immediate:true,  //1.是否在页面加载完毕后，立即执行一次 handler(newVal ,oldVal) 函数
-            deep:true,      //2.深度侦听复杂类型内属性的变化
-            handler(newVal,oldVal){
+            immediate:true,  //1. immediate 属性用来声明此侦听器，是否在页面初次加载完毕后立即调用 handler(newVal ,oldVal) 函数
+
+            deep:true,      //2.深度侦听复杂引用类型内属性的变化
+
+            handler(newVal,oldVal){ //3.handler 属性用来定义侦听器的 function 处理函数
                 //打印的是user对象
                 console.log(newVal,oldVal)
             } 
